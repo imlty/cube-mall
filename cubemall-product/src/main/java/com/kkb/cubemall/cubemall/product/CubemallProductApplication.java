36178e04091ff73373b2e98e68e9ae15720595b3
imlty <imlty626@qq.com>
2021-09-07 22:48:11 +0800

    init : 初始化商品微服务模块

---
+++ cubemall-product/src/main/java/com/kkb/cubemall/cubemall/product/CubemallProductApplication.java
@@ -0,0 +1,13 @@
+package com.kkb.cubemall.cubemall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubemallProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubemallProductApplication.class, args);
+	}
+
+}
---
+++ cubemall-product/src/test/java/com/kkb/cubemall/cubemall/product/CubemallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkb.cubemall.cubemall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubemallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
