e0a00f1a630dbe46da7f0abd29ee4345f35f70ff
imlty <imlty626@qq.com>
2021-09-30 15:07:43 +0800

    feat: 1. add cubemall-common

---
+++ cubemall-gateway/src/main/java/com/kkb/cubemall/lgateway/CubemallGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kkb.cubemall.lgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubemallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubemallGatewayApplication.class, args);
+    }
+
+}
---
+++ cubemall-gateway/src/test/java/com/kkb/cubemall/lgateway/CubemallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkb.cubemall.lgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubemallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
