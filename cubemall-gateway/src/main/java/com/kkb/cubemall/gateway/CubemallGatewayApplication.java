50ce8d00c2947ae83267cb44052493d19a1b2658
imlty <imlty626@qq.com>
2021-09-30 15:27:10 +0800

    feat: add CubemallGatewayApplication

--- cubemall-gateway/src/main/java/com/kkb/cubemall/gateway/CubemallGatewayApplication.java
+++ cubemall-gateway/src/main/java/com/kkb/cubemall/gateway/CubemallGatewayApplication.java
@@ -1,9 +1,12 @@
-package com.kkb.cubemall.lgateway;
+package com.kkb.cubemall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class CubemallGatewayApplication {
 
     public static void main(String[] args) {
