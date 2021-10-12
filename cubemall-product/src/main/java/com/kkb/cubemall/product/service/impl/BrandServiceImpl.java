81c51b33fa9733dac52c6b41c126bbb301b5f844
yangwenbo <yangwb@actwill.com.cn>
2021-10-12 17:43:14 +0800

    feat : add query brand by id and name

--- cubemall-product/src/main/java/com/kkb/cubemall/product/service/impl/BrandServiceImpl.java
+++ cubemall-product/src/main/java/com/kkb/cubemall/product/service/impl/BrandServiceImpl.java
@@ -1,16 +1,17 @@
 package com.kkb.cubemall.product.service.impl;
 
-import com.kkb.cubemall.common.utils.PageUtils;
-import com.kkb.cubemall.common.utils.Query;
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-
+import com.kkb.cubemall.common.utils.PageUtils;
+import com.kkb.cubemall.common.utils.Query;
 import com.kkb.cubemall.product.dao.BrandDao;
 import com.kkb.cubemall.product.entity.BrandEntity;
 import com.kkb.cubemall.product.service.BrandService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("brandService")
@@ -18,9 +19,23 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        // 编写条件查询的条件
+        String key = (String) params.get("key");
+
+        // 封装查询条件
+        QueryWrapper<BrandEntity> brandEntityQueryWrapper = new QueryWrapper<>();
+        if (StringUtils.isNotEmpty(key)) {
+            brandEntityQueryWrapper
+                    .eq("id", key)
+                    .or()
+                    .like("name", key);
+        }
+        //  SELECT COUNT(*) FROM tb_brand WHERE (id = ? OR name LIKE ?)
+        //  SELECT id,name,image,letter,seq FROM tb_brand WHERE (id = ? OR name LIKE ?) LIMIT ?
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                brandEntityQueryWrapper
         );
 
         return new PageUtils(page);
