b0829e904f57ff578acbdc9fbce5ffc1e436354d
yangwenbo <yangwb@actwill.com.cn>
2021-10-19 16:52:23 +0800

    feat: select AttrGroup by params

--- cubemall-product/src/main/java/com/kkb/cubemall/product/controller/AttrGroupController.java
+++ cubemall-product/src/main/java/com/kkb/cubemall/product/controller/AttrGroupController.java
@@ -33,11 +33,11 @@ public class AttrGroupController {
     /**
      * 列表
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/{categoryId}")
     //@RequiresPermissions("product:attrgroup:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = attrGroupService.queryPage(params);
-
+    public R list(@RequestParam Map<String, Object> params,@PathVariable("categoryId") Long categoryId){
+        // PageUtils page = attrGroupService.queryPage(params);
+        PageUtils page = attrGroupService.queryPage(params,categoryId);
         return R.ok().put("page", page);
     }
 
--- cubemall-product/src/main/java/com/kkb/cubemall/product/service/impl/AttrGroupServiceImpl.java
+++ cubemall-product/src/main/java/com/kkb/cubemall/product/service/impl/AttrGroupServiceImpl.java
@@ -1,16 +1,17 @@
 package com.kkb.cubemall.product.service.impl;
 
-import com.kkb.cubemall.common.utils.PageUtils;
-import com.kkb.cubemall.common.utils.Query;
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-
+import com.kkb.cubemall.common.utils.PageUtils;
+import com.kkb.cubemall.common.utils.Query;
 import com.kkb.cubemall.product.dao.AttrGroupDao;
 import com.kkb.cubemall.product.entity.AttrGroupEntity;
 import com.kkb.cubemall.product.service.AttrGroupService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("attrGroupService")
@@ -24,6 +25,49 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 获取指定分类下的所有属性分组列表
+     *
+     * @param params
+     * @param categoryId
+     * @return
+     */
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long categoryId) {
+        if (categoryId == 0) {
+            String key = (String) params.get("key");
+            QueryWrapper<AttrGroupEntity> wrapper = new QueryWrapper<>();
+            // 查询条件： key 【名称】
+            if (StringUtils.isNotEmpty(key)) {
+                wrapper.and(obj -> obj.eq("id", key).or().like("name", key));
+            }
+            IPage<AttrGroupEntity> page = this.page(
+                    // 分页条件
+                    new Query<AttrGroupEntity>().getPage(params),
+                    // 查询条件
+                    wrapper
+            );
+            return new PageUtils(page);
+        } else {
+            // 查询指定分类下的所有属性分组信息 【包括 categoryId 和 parmas.key】
+            String key = (String) params.get("key");
+            QueryWrapper<AttrGroupEntity> wrapper = new QueryWrapper<>();
+            // 查询条件1： categoryId
+            // 查询条件2： key 【名称】
+            wrapper.eq("category_id", categoryId);
+            if (StringUtils.isNotEmpty(key)) {
+                wrapper.and(obj -> obj.eq("id", key).or().like("name", key));
+            }
+            IPage<AttrGroupEntity> page = this.page(
+                    // 分页条件
+                    new Query<AttrGroupEntity>().getPage(params),
+                    // 查询条件
+                    wrapper
+            );
+            return new PageUtils(page);
+        }
     }
 
 }
