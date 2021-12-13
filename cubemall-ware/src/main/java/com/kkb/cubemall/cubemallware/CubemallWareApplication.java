97b6567e8d4e1f8cc0eedeeea22f79edeb8def36
yangwenbo <yangwb@actwill.com.cn>
2021-12-13 14:51:32 +0800

    feat: add cubemall-ware product

---
+++ cubemall-ware/src/main/java/com/kkb/cubemall/cubemallware/CubemallWareApplication.java
@@ -0,0 +1,13 @@
+package com.kkb.cubemall.cubemallware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubemallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubemallWareApplication.class, args);
+    }
+
+}
---
+++ cubemall-ware/src/test/java/com/kkb/cubemall/cubemallware/CubemallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkb.cubemall.cubemallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubemallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
